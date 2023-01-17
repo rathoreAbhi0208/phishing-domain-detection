bd2c30c2d535783385e7dfffb55d7ede50631181
Abhishek Kumar Singh <awikumar830@gmail.com>
2023-01-17 12:16:05 +0530

    dump

--- data_dump.py
+++ data_dump.py
@@ -3,7 +3,7 @@ import pandas as pd
 import json
 
 
-client = pymongo.MongoClient("mongodb+srv://Abhi:Abhi02@cluster0.ozaps66.mongodb.net/?retryWrites=true&w=majority")
+client = pymongo.MongoClient(MONGO_CLIENT)
 
 DATA_FILE_PATH = "D:\Python\Inue\phishing_domain_detection\phishing_dataset.csv"
 DATABASE_NAME = 'phishing'
@@ -14,9 +14,11 @@ if __name__ =="__main__":
     df=pd.read_csv(DATA_FILE_PATH)
     print(f"Rows and Columns: {df.shape}")
 
+    #convert dataframe to json so that we can dump these record in mongoDb
     df.reset_index(drop=True,inplace=True)
 
     json_record = list(json.loads(df.T.to_json()).values())
     print(json_record[0])
 
+    #insert converted data to mongoDb
     client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
