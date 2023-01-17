868efcae0dd3ac0c0ce832a2e0f0af6e41c74fcb
Abhishek Kumar Singh <awikumar830@gmail.com>
2023-01-17 20:27:13 +0530

    Exception Module Added

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-print('main')
+from phishing.logger import logging
+from phishing.exception import PhishingException
+import os ,sys
+
+# def test_logger_and_exception():
+#     try:
+#         logging.info("Starting the test_logger_and_exception")
+#         result =3/0
+#         print(result)
+#         logging.info("Stopping test_logger_and_exception")
+#     except Exception as e:
+
+#         logging.debug(str(e))
+#         raise PhishingException(e, sys)
 
 
-if __name__ =="__main__":
-    pass
+if __name__ == "__main__":
+    try:
+        #test_logger_and_exception()
+        pass
+    except Exception as e:
+        print(e)
