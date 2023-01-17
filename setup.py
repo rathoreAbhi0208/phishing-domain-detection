f86ebe15c7d8ef0595de412425b0279db7f8b201
Abhishek Kumar Singh <awikumar830@gmail.com>
2023-01-17 19:17:30 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+
+def get_requirements():
+    pass
+
+setup(
+    name="phishing",
+    version="0.0.1",
+    author = "abhi",
+    packages = find_packages(),
+    install_requires = get_requirements(),
+)
